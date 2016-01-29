bf65aca323c7a4589705bf0b6bfe2f2463467c5d
Adetunji Dahunsi <tjdah100@gmail.com>
2016-01-29 14:06:44 -0500

    Added private IP for prod

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ var app = express();
 passport();
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'));
 
 
-console.log('Server listening on ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
+if (process.env.NODE_ENV === 'development') {
+    app.listen(app.get('port'), function () {
+        console.log('Server listening on ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
+    });
+}
+else if (process.env.NODE_ENV === 'production') {
+    app.listen(app.get('port'), '10.128.9.90', function () {
+        console.log('Server listening on ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
+    });
+}
+
 
 module.exports = app;
