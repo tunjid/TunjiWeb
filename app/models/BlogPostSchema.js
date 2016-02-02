4e54fc404ee3a93b4971d3110fa984075aebb624
Adetunji Dahunsi <tjdah100@gmail.com>
2016-02-01 20:06:36 -0500

    Added default values for tags aand categories.

--- app/models/BlogPostSchema.js
+++ app/models/BlogPostSchema.js
@@ -6,8 +6,8 @@ var BlogPostSchema = new Schema({
     author: {type: Schema.ObjectId, ref: 'User', required: 'Author is required',},
     link: {type: String, default: '/blog/' + Date.prototype.stringDate()},
     body: String,
-    tags: {type:[String], index: true},
-    categories: {type:[String], index: true},
+    tags: {type:[String], index: true, default:['untagged']},
+    categories: {type:[String], index: true, default:['uncategorized']},
     comments: [{body: String, date: Date}],
     stringDate: {type: String, default: Date.prototype.getDateTimeFormat()},
     created: {type: Date, default: Date.now}
