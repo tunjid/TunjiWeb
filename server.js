520c1e3d9f8561d0fd0645c4a629f40e9d1eb90d
Adetunji Dahunsi <tjdah100@gmail.com>
2018-05-05 12:54:17 -0400

    Removed duplication of port

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = require('./config/config').serverEnvironment;
 
 var express = require('./config/express');
 var passport = require('./config/passport');
 
 var app = express();
 passport();
-
-app.set('port', 3000);
-
-
-if (process.env.NODE_ENV === 'development') {
-    app.listen(app.get('port'), function () {
-        console.log('Server listening on ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
-    });
-}
-else if (process.env.NODE_ENV === 'production') {
-    app.listen(app.get('port'), '10.128.9.90', function () {
-        console.log('Server listening on ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
-    });
-}
-
 
 module.exports = app;
